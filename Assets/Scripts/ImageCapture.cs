b217743d3ec52c3ee2eb58c205055585774ed2a3
Clive Lee <hyungulee@gmail.com>
2019-01-19 10:30:14 -0500

    unified VS solution & deployed to device

---
+++ Assets/Scripts/ImageCapture.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.XR.WSA.Input;
+using UnityEngine.XR.WSA.WebCam;
+
+public class ImageCapture : MonoBehaviour {
+
+    public static ImageCapture instance;
+    public int tapsCount;
+    private PhotoCapture photoCaptureObject = null;
+    private GestureRecognizer recognizer;
+    private bool currentlyCapturing = false;
+
+    private void Awake()
+    {
+        // Allows this instance to behave like a singleton
+        instance = this;
+    }
+
+    void Start()
+    {
+        // subscribing to the Hololens API gesture recognizer to track user gestures
+        recognizer = new GestureRecognizer();
+        recognizer.SetRecognizableGestures(GestureSettings.Tap);
+        recognizer.Tapped += TapHandler;
+        recognizer.StartCapturingGestures();
+    }
+
+    /// <summary>
+    /// Respond to Tap Input.
+    /// </summary>
+    private void TapHandler(TappedEventArgs obj)
+    {
+        // Only allow capturing, if not currently processing a request.
+        if (currentlyCapturing == false)
+        {
+            currentlyCapturing = true;
+
+            // increment taps count, used to name images when saving
+            tapsCount++;
+
+            // Create a label in world space using the ResultsLabel class
+            ResultsLabel.instance.CreateLabel();
+
+            // Begins the image capture and analysis procedure
+            //ExecuteImageCaptureAndAnalysis();
+        }
+    }
+}
