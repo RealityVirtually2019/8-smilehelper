172cf0e30fbe9348930264324373c3f571ddad09
Erin Woo <erinpwoo@umail.ucsb.edu>
2019-01-19 13:39:09 -0500

    TriggerEvents script for event-based behavior

---
+++ Assets/Scripts/TriggerEvents.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using UnityEngine;
+
+public class TriggerEvents : MonoBehaviour {
+
+    public interface ICustomMessageTarget : IEventSystemHandler
+    {
+        // functions that can be called via the messaging system
+        void TestMessage1();
+    }
+
+
+    //anything extending ICustomMessageTarget can implement custom behavior upon receiving message.
+   
+    public class MessageTarget1 : MonoBehaviour, ICustomMessageTarget
+    {
+        public void TestMessage1()
+        {
+            Debug.Log("Message Received");
+        }
+    }
+}
+
+//this code will execute the function TestMessage1 on any components on the GameObject target that implement the ICustomMessageTarget interface
+//ExecuteEvents.Execute<ICustomMessageTarget>(target, null, (x,y)=>x.Message1());
